781bafbd98938be74c3ba30de4b43b112edf5be1
jthw1005 <skl0105@naver.com>
2022-07-01 00:36:33 +0900

    design: Label component UI 구현

--- FE/src/pages/Label/index.tsx
+++ FE/src/pages/Label/index.tsx
@@ -1,5 +1,50 @@
+import { useMutation, useQuery } from "react-query";
+
+import API from "@/apis";
+import { LabelType } from "@/apis/type";
+import ListLayout from "@/components/common/ListLayout";
+import Header from "@/components/LabelMilestone/Header";
+
+import * as S from "./style";
+
 const LabelPage = () => {
-  return <div>Label</div>;
+  const { isLoading, data } = useQuery("issue-labels", API.getLabelList);
+
+  const LabelListComponents = data?.data.map((el: LabelType) => {
+    const handleClickLabelEdit = () => {};
+    const handleClickLabelDelete = () => {};
+
+    if (isLoading) {
+      return <h2>Loading...</h2>;
+    }
+
+    return (
+      <S.LabelListBox key={el.labelId}>
+        <S.FrontBox>
+          <S.LabelBoxWrapper>
+            <S.LabelBox backgroundColor={el.backgroundColor} color={el.fontColor}>
+              {el.name}
+            </S.LabelBox>
+          </S.LabelBoxWrapper>
+          <S.LabelDescription>{el.description}</S.LabelDescription>
+        </S.FrontBox>
+        <S.BackBox>
+          <button>편집</button>
+          <button>삭제</button>
+        </S.BackBox>
+      </S.LabelListBox>
+    );
+  });
+
+  const numOfLabel = data?.data.length;
+  const LabelListTitle = () => <div>{`${numOfLabel}개의 레이블`}</div>;
+
+  return (
+    <>
+      <Header />
+      <ListLayout width={1280} titleComponent={<LabelListTitle />} listComponents={LabelListComponents} />
+    </>
+  );
 };
 
 export default LabelPage;
--- FE/src/pages/Milestone/index.tsx
+++ FE/src/pages/Milestone/index.tsx
@@ -1,5 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+import Button from "@/components/common/Button";
+import FilterButton from "@/components/common/FilterButton";
+import Icon from "@/components/common/Icon";
+import Header from "@/components/LabelMilestone/Header";
+import { COLOR, FONTSIZE, FONTWEIGHT } from "@/styles/constTheme";
+
+import * as S from "./style";
+
 const MilestonePage = () => {
-  return <div>Milestone</div>;
+  return (
+    <>
+      <Header />
+      <div>This is Milestone</div>
+    </>
+  );
 };
 
 export default MilestonePage;
